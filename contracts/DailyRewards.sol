f82590f64638f90736a04c15b4ec0e69f25ab11e
Israel Levin <israel@deweb.io>
2021-03-11 09:53:07 +0200

    Basic rewards setting

---
+++ contracts/DailyRewards.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.8.0;
+
+/**
+ * @title Reward
+ * @dev Set and distribute daily rewards.
+ */
+contract DailyRewards {
+
+    uint256 public constant DECLARATION_INTERVAL = 10 seconds;
+    uint256 public constant REWARD_INTERVAL = 10 seconds;
+
+    struct Reward {
+        address beneficiary;
+        uint256 amountBBS;
+    }
+    address owner;
+    Reward[] public rewards;
+    Reward[] public declaredRewards;
+    uint256 public declarationTimestamp;
+    uint256 public lastDistributionTimestamp;
+
+    /**
+     * @dev Set Owner
+     */
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Declare the daily rewards before they are set.
+     * @param addressesToSet A list of addresses to reward.
+     * @param rewardsToSet A mapping of the reward to give addresses.
+     */
+    function declareReward(address[] memory addressesToSet, uint256[] memory rewardsToSet) external {
+        require(msg.sender == owner, "non-owner reward declaration");
+        delete declaredRewards;
+        for (uint256 i = 0; i < addressesToSet.length; i++) {
+            declaredRewards.push(Reward(addressesToSet[i], rewardsToSet[i]));
+        }
+        declarationTimestamp = block.timestamp;
+    }
+
+     /**
+     * @dev Set the daily rewards as they were declared.
+     */
+    function setReward() external {
+        require(declarationTimestamp > 0, "no rewards declared");
+        require(block.timestamp - declarationTimestamp >= DECLARATION_INTERVAL, "rewards declared too recently");
+        rewards = declaredRewards;
+        declarationTimestamp = 0;
+    }
+}
