ad43f1f544498d9bd6bf8b17ee12aa284b706620
Israel Levin <israel@deweb.io>
2021-03-11 17:38:34 +0200

    Deploy with hardhat

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 This repository will contain all components of the BBS token. This includes the token contracts themselves, the cross-blockchain bridge, and some components of the BBS system governance.
 
+- To test on hardhat network:
 ```shell
 npm install
 npx hardhat test
+```
+
+- To deploy locally:
+```shell
+npx ganache-cli &
+npx hardhat run scripts/deploy.js --network localhost
+```
+
+- To deploy remotely:
+```shell
+npx hardhat run scripts/deploy.js --network <network name>
 ```
 
 ## Token
---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+async function main() {
+    const Token = await hre.ethers.getContractFactory("BBSToken");
+    const token = await Token.deploy(100);
+    await token.deployed();
+    console.log("token deployed to:", token.address);
+
+    const Rewards = await hre.ethers.getContractFactory("DailyRewards");
+    const rewards = await Rewards.deploy();
+    await rewards.deployed();
+    console.log("rewards deployed to:", rewards.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
