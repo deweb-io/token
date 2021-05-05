7628baa17b0ed5951e203a4ff4867a13696e7d9c
yanivy <yanivy@freeports.dev>
2021-05-05 17:52:28 +0300

    support running tests with already deployed contracts

--- test/liquidityMining-test.js
+++ test/liquidityMining-test.js
@@ -1,16 +1,28 @@
 const { expect } = require('chai');
+const fs = require('fs');
 const chai = require('chai');
 chai.use(require('chai-string'));
-
+const { ethers } = require('hardhat');
 const Web3 = require('web3');
-const web3Abi = new Web3().eth.abi;
-const web3Utils = new Web3().utils;
+
+const web3 = new Web3();
+const web3Abi = web3.eth.abi;
+const web3Utils = web3.utils;
+
+const abis = {
+    registry: JSON.parse(fs.readFileSync('./abis/ContractRegistry.abi', 'utf8')),
+    liquidityProtection: JSON.parse(fs.readFileSync('./abis/LiquidityProtection.abi', 'utf8')),
+    liquidityProtectionStore: JSON.parse(fs.readFileSync('./abis/LiquidityProtectionStore.abi', 'utf8'))
+};
 
 const BBS_INIT_STACKING = 100;
 const MAX_ACCOUNTS = 10;
 
+/**
+ * for example: BANCOR_ENV_REGISTRY="0x5c8152554B3c8F39c3F8d1dEe141cCeA4914517A" npx hardhat --network localhost test liquidityMining-test.js
+ */
+const BANCOR_ENV_REGISTRY = process.env.BANCOR_ENV_REGISTRY;
 
-//TODO: add env variable for contract registry
 
 describe('LiquidityMining', function() {
     let startTime;
@@ -19,53 +31,78 @@ describe('LiquidityMining', function() {
     let bbsToken;
     let LiquidityMining;
     let liquidityMining;
+    let contractRegistry;
+    let liquidityProtection;
+    let liquidityProtectionStore;
 
     beforeEach(async function() {
         startTime = Math.floor(new Date / 1000);
         currentTime = startTime;
         accounts = (await ethers.getSigners()).slice(0, MAX_ACCOUNTS);
-        
+
+        //deploy BBS token
         BBSToken = await ethers.getContractFactory('BBSToken');
         bbsToken = await BBSToken.deploy();
-        
-        LiquidityProtectionStore = await ethers.getContractFactory('mockLiquidityProtectionStore');
-        liquidityProtectionStore = await LiquidityProtectionStore.deploy();
 
-        await Promise.all(accounts.map(async (account, index) => {
-            await liquidityProtectionStore.addProtectedLiquidity(
-                    account.address,
-                    accounts[8].address,
-                    accounts[9].address,
-                    1,
-                    BBS_INIT_STACKING,
-                    1,
-                    1,
-                    1);
-            console.log(`account ${index} stacking: <${accounts[index].address},${BBS_INIT_STACKING}>`);
-        }));
+        if (BANCOR_ENV_REGISTRY) {
+            console.log('running tests on localhost');
+            //get deployed contracts
+            contractRegistry = await ethers.getContractAt(abis.registry, BANCOR_ENV_REGISTRY);
+            liquidityProtectionAddress = await contractRegistry.addressOf(ethers.utils.formatBytes32String('LiquidityProtection'));
+            liquidityProtection = await ethers.getContractAt(abis.liquidityProtection, liquidityProtectionAddress);
+            liquidityProtectionStoreAddress = await contractRegistry.addressOf(ethers.utils.formatBytes32String('LiquidityProtectionStore'));
+            liquidityProtectionStore = await ethers.getContractAt(abis.liquidityProtectionStore, liquidityProtectionStoreAddress);
+ 
+            //deploy liquidityMining contract
+            LiquidityMining = await ethers.getContractFactory('LiquidityMining');
+            liquidityMining = await LiquidityMining.deploy(bbsToken.address, BANCOR_ENV_REGISTRY);
+        } else {
+            console.log('running tests on in-memory instance hardhat network');
+            LiquidityProtectionStore = await ethers.getContractFactory('mockLiquidityProtectionStore');
+            liquidityProtectionStore = await LiquidityProtectionStore.deploy();
 
-        LiquidityProtection = await ethers.getContractFactory('mockLiquidityProtection');
-        liquidityProtection = await LiquidityProtection.deploy(liquidityProtectionStore.address);
+            await Promise.all(accounts.map(async (account, index) => {
+                await liquidityProtectionStore.addProtectedLiquidity(
+                        account.address,
+                        accounts[8].address,
+                        accounts[9].address,
+                        1,
+                        BBS_INIT_STACKING,
+                        1,
+                        1,
+                        1);
+                console.log(`account ${index} stacking: <${accounts[index].address},${BBS_INIT_STACKING}>`);
+            }));
 
-        ContractRegistry = await ethers.getContractFactory('mockContractRegistry');
-        contractRegistry = await ContractRegistry.deploy();
-        
-        await contractRegistry.addContract(liquidityProtection.address, 
-            web3Abi.encodeParameter('bytes32', web3Utils.utf8ToHex('LiquidityProtection')));
-        await contractRegistry.addContract(liquidityProtectionStore.address, 
-            web3Abi.encodeParameter('bytes32', web3Utils.utf8ToHex('LiquidityProtectionStore')));
-        
-        LiquidityMining = await ethers.getContractFactory('LiquidityMining');
-        liquidityMining = await LiquidityMining.deploy(bbsToken.address, contractRegistry.address);
+            LiquidityProtection = await ethers.getContractFactory('mockLiquidityProtection');
+            liquidityProtection = await LiquidityProtection.deploy(liquidityProtectionStore.address);
 
-        printContractDetails('LiquidityProtection', liquidityProtection.address);
-        printContractDetails('LiquidityProtectionStore', liquidityProtectionStore.address);
-        printContractDetails('ContractRegistry', contractRegistry.address);
-        printContractDetails('LiquidityMining', liquidityMining.address);
+            ContractRegistry = await ethers.getContractFactory('mockContractRegistry');
+            contractRegistry = await ContractRegistry.deploy();
+            
+            await contractRegistry.addContract(liquidityProtection.address, 
+                web3Abi.encodeParameter('bytes32', web3Utils.utf8ToHex('LiquidityProtection')));
+            await contractRegistry.addContract(liquidityProtectionStore.address, 
+                web3Abi.encodeParameter('bytes32', web3Utils.utf8ToHex('LiquidityProtectionStore')));
+            
+            LiquidityMining = await ethers.getContractFactory('LiquidityMining');
+            liquidityMining = await LiquidityMining.deploy(bbsToken.address, contractRegistry.address);
+        }
+        printContracts();
     });
 
+    function printContracts() {
+        console.log('####### Contracts #######');
+        printContractDetails('ContractRegistry', contractRegistry.address);
+        printContractDetails('LiquidityMining', liquidityMining.address);
+        printContractDetails('LiquidityProtection', liquidityProtection.address);
+        printContractDetails('LiquidityProtectionStore', liquidityProtectionStore.address);
+        printContractDetails('BBS token', bbsToken.address);
+        console.log('#########################');
+    }
+
     function printContractDetails(name, addrees) {
-        console.log(`${name} --> ${addrees}`);
+        console.log(`${name} is deployed at ${addrees}`);
     }
 
     function getDaysTillTimestamp(timestamp, since) {
@@ -101,10 +138,6 @@ describe('LiquidityMining', function() {
                 positionId, numberOfDays, returnAddress
             ]
         );
-    }
-
-    async function printBalance(accountIndex) {
-        //console.log(`account ${accountIndex} has ${await getBalance(accounts[accountIndex].address)} bbs`);
     }
 
     function printRewards(accountIndex, rewards) {
@@ -165,7 +198,6 @@ describe('LiquidityMining', function() {
         const numOfDays = 100;
         const totalBBSRewards = 1;
         const positionId = 0;
-        await printBalance(0);
         await transferPositionAndCallWrapper(positionId, numOfDays);
         await sendBBSToLM(totalBBSRewards);
         await increaseTime(numOfDays);
@@ -178,7 +210,6 @@ describe('LiquidityMining', function() {
         const numOfDays = 998;
         const totalBBSRewards = 1000;
         const positionId = 0;
-        await printBalance(0);
         await transferPositionAndCallWrapper(positionId, numOfDays);
         await sendBBSToLM(totalBBSRewards);
         await increaseTime(numOfDays);
@@ -190,8 +221,6 @@ describe('LiquidityMining', function() {
     it('accounts with the same BBS stacking and lock time should get the same rewards', async function() {
         const numOfDays = 100;
         const totalBBSRewards = 14321;
-        await printBalance(0);
-        await printBalance(1);
         await transferPositionAndCallWrapper(0, numOfDays);
         await transferPositionAndCallWrapper(1, numOfDays);
         await sendBBSToLM(totalBBSRewards);
@@ -208,8 +237,6 @@ describe('LiquidityMining', function() {
     it('account with longer lock time should get more rewards', async function() {
         const numOfDays = 200;
         const bbsRewards = 5000;
-        await printBalance(0);
-        await printBalance(1);
         await transferPositionAndCallWrapper(0, numOfDays);
         await transferPositionAndCallWrapper(1, numOfDays-5);
         await sendBBSToLM(bbsRewards);
@@ -226,8 +253,6 @@ describe('LiquidityMining', function() {
     it('no rewards if no balance in lm contract', async function() {
         const numOfDays = 200;
         const totalBbsRewards = 5000;
-        await printBalance(0);
-        await printBalance(1);
         await transferPositionAndCallWrapper(0, numOfDays);
         await transferPositionAndCallWrapper(1, numOfDays-5);
         await sendBBSToLM(totalBbsRewards);
@@ -236,7 +261,6 @@ describe('LiquidityMining', function() {
         await liquidityMining.unlockPosition(1);
         printRewards(0, (await getBalance(accounts[0].address)));
         printRewards(1, (await getBalance(accounts[1].address)));
-
         await liquidityMining.lockPosition(2, numOfDays, accounts[0].address);
         await increaseTime(numOfDays);
         await liquidityMining.unlockPosition(2);
@@ -247,7 +271,6 @@ describe('LiquidityMining', function() {
     it('account lock&unlock several times', async function() {
         const numOfDays = 100;
         let totalBbsRewards = 5000;
-        await printBalance(0);
         await transferPositionAndCallWrapper(0, numOfDays);
         await sendBBSToLM(totalBbsRewards);
         await increaseTime(numOfDays);
