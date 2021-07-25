3d61e76b31b4f6dcd8570cdea6d1041daa756570
tomerer <tomer@deweb.io>
2021-07-25 18:56:28 +0300

    bbs and bancor x deployment + tests

--- test/bridge-test.js
+++ test/bridge-test.js
@@ -1,27 +1,17 @@
 const { ethers } = require('hardhat');
-const fs = require('fs');
-const hardhat = require('hardhat');
-const path = require('path');
 const Web3 = require('web3');
-const web3 = new Web3('http://localhost:8545');
+// const web3 = new Web3('http://localhost:8545');
+const web3 = new Web3('https://ropsten.infura.io/v3/b481942a6a15462988f7ab9000ca51ab');
 
 
-// These are ABIs were generated from the Bancor contracts, so we have full computability.
-// We will probably have a script that generates them automatically from a given version.
-const abis = {
-    registry: JSON.parse(fs.readFileSync(path.join(hardhat.config.paths.tests, './abis/ContractRegistry.abi'))),
-    bancorX: JSON.parse(fs.readFileSync(path.join(hardhat.config.paths.tests, './abis/BancorX.abi')))
-};
 
 // Set this environment variables to use previously deployed contracts.
-const REGISTRY = process.env.BANCOR_ENV_REGISTRY || '0x4e3df2073bf4b43B9944b8e5A463b1E185D6448C';
-const BBS_TOKEN = process.env.BANCOR_ENV_BBS_TOKEN || '0xF2E246BB76DF876Cef8b38ae84130F4F55De395b';
-const BANCOR_X = process.env.BANCOR_ENV_BANCOR_X || '0xf8466c6af264DDA1c5b4EEcDAE416Fd708DeB3e7';
+const BBS_TOKEN = '0x94F32CA9c737FFe1b9e040de4027BAB92eb1f85a';
+const BANCOR_X = '0x9db840EfaA31Be39e46E9782566D8f20ACfFE9cb';
 
 describe('Bridge', function() {
     let bbsToken;
     let bancorX;
-    let contractRegistry;
     let signer;
 
     function printContractDetails(name, addrees) {
@@ -30,7 +20,6 @@ describe('Bridge', function() {
 
     function printContracts() {
         console.log('####### Contracts #######');
-        printContractDetails('ContractRegistry', contractRegistry && contractRegistry.address);
         printContractDetails('BancorX', bancorX && bancorX.address);
         printContractDetails('BBS token', bbsToken && bbsToken.address);
         console.log('#########################');
@@ -39,24 +28,25 @@ describe('Bridge', function() {
     beforeEach(async function() {
         signer = (await ethers.getSigners())[0];
         bbsToken = await ethers.getContractAt('BBSToken', BBS_TOKEN);
-        contractRegistry = await ethers.getContractAt(abis.registry, REGISTRY);
-        bancorX = await ethers.getContractAt(abis.bancorX, BANCOR_X);
+        bancorX = await ethers.getContractAt('BancorX', BANCOR_X);
         printContracts();
     });
 
-    it('min bbs and make x transfer', async function() {
+    it('mint BBS and make x transfer', async function() {
         // const topic = web3.utils.keccak256('XTransfer(address,bytes32,bytes32,uint256,uint256)');
         // console.log(topic);
         // console.log(await web3.eth.getPastLogs({topics: [topic]}));
 
-        console.log('address', signer.address);
+
+        console.log('signer address', signer.address);
         await bbsToken.mint(signer.address, ethers.utils.parseEther('1'));
+        console.log('BBS minted');
 
         const ethBalance = await web3.eth.getBalance(signer.address);
-        console.log(ethBalance);
+        console.log('eth balance', ethBalance);
 
         const balance = (await bbsToken.balanceOf(signer.address));
-        console.log('balance', balance);
+        console.log('BBS balance', balance);
 
         const eosBlockchain = ethers.utils.formatBytes32String('eos'); 
         console.log('eosBlockchain',eosBlockchain);
@@ -66,7 +56,7 @@ describe('Bridge', function() {
         console.log(amount);
 
         await bbsToken.connect(signer).approve(bancorX.address, amount);
-        const xTransfer = await bancorX.connect(signer)['xTransfer(bytes32,bytes32,uint256,uint256)'](eosBlockchain, eosAddress, amount, 5454);
+        const xTransfer = await bancorX.connect(signer)['xTransfer(bytes32,bytes32,uint256,uint256)'](eosBlockchain, eosAddress, amount, 7343, {from: signer.address});
         console.log(xTransfer);
     });
 });
