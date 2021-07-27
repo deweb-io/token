d317213f61253a8df0157e6c081bc7756dee725e
Israel Levin <israel@deweb.io>
2021-07-27 10:20:26 +0300

    Some cleanup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,10 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 require('@nomiclabs/hardhat-etherscan');
 require('@openzeppelin/hardhat-upgrades');
 require('hardhat-gas-reporter');
 require('solidity-coverage');
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
     solidity: {
         version: '0.8.6',
@@ -24,12 +24,7 @@ module.exports = {
     networks: {
         ropsten: {
             url: 'https://ropsten.infura.io/v3/b481942a6a15462988f7ab9000ca51ab',
-            // Public-key:0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
             accounts : ['0xa54eb7f94dae3fdf83bdb0036eea1c673e824fc8109a70bdb6748adddffe3538']
-        },
-        localhost: {
-            url: 'http://127.0.0.1:8545',
-            accounts : ['0x0000000000000000000000000000000000000000000000000000000000000001']
         }
     }
 };
