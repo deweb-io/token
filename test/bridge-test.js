6499a411ca78e50cd01c34b6e15285dc6d576a20
tomerer <tomer@deweb.io>
2021-07-27 16:07:57 +0300

    add report tx test

--- test/bridge-test.js
+++ test/bridge-test.js
@@ -8,7 +8,10 @@ const BANCOR_X = '0x9db840EfaA31Be39e46E9782566D8f20ACfFE9cb';
 describe('Bridge', function() {
     let bbsToken;
     let bancorX;
+    let accounts;
     let signer;
+    let reporter;
+    let xtransferAmount = ethers.utils.parseEther('5');
 
     function printContractDetails(name, addrees) {
         console.log(`${name} is deployed at ${addrees}`);
@@ -22,9 +25,12 @@ describe('Bridge', function() {
     }
 
     beforeEach(async function() {
+        accounts = await ethers.getSigners();
+        signer = accounts[0];
+        reporter = accounts[1];
+
         const Token = await ethers.getContractFactory('BBSToken');
         bbsToken = await Token.deploy();
-        console.log(`BBS token deployed at ${bbsToken.address}`);
 
         const BancorX = await ethers.getContractFactory('BancorX');
         bancorX = await BancorX.deploy(
@@ -36,34 +42,55 @@ describe('Bridge', function() {
             '0x9eED1767B3c33D4A4fDB7c76070DE2dDfd37e808', // should be upgrader contract address
             bbsToken.address);
 
-        console.log(`BancorX deployed at ${bancorX.address}`);
 
         // const REPORTER1_PRIVATE_KEY = '0xe427e1a30d344c90f0b3884df1e58273ee7b6084c055bcc84090e2915967d8c6';
-        const reporterAddress = '0x41C87AC77a3ec4E192F0f3a0c598f8027Ec16177';
-        await bancorX['setReporter(address,bool)'](reporterAddress, true);
-        console.log(`set reporter on bancor x ${reporterAddress}`);
+        // const reporterAddress = '0x41C87AC77a3ec4E192F0f3a0c598f8027Ec16177';
+        await bancorX['setReporter(address,bool)'](reporter.address, true);
+        console.log(`set reporter on bancor x ${reporter.address}`);
 
-        signer = (await ethers.getSigners())[0];
+        await bbsToken.connect(signer).approve(bancorX.address, xtransferAmount);
         printContracts();
     });
 
     it('mint BBS and make x transfer', async function() {
         console.log('signer address', signer.address);
 
-        const mintAmount = ethers.utils.parseEther('10');
+        const mintAmount = ethers.utils.parseEther('5');
         await bbsToken.mint(signer.address, mintAmount);
         console.log('BBS minted', mintAmount);
 
-        const balance = (await bbsToken.balanceOf(signer.address));
-        console.log('BBS balance', balance);
+        const balanceBefore = (await bbsToken.balanceOf(signer.address));
+        console.log('BBS balance before x transfer', balanceBefore);
 
         const eosBlockchain = ethers.utils.formatBytes32String('eos'); 
         const eosAddress = ethers.utils.formatBytes32String('tomeraccount'); 
-        const txId = Math.floor(Math.random() * (100000));
+        const xtransferId = Math.floor(Math.random() * (100000));
 
-        const xtransferAmount = ethers.utils.parseEther('5');
-        await bbsToken.connect(signer).approve(bancorX.address, xtransferAmount);
-        const xTransfer = await bancorX.connect(signer)['xTransfer(bytes32,bytes32,uint256,uint256)'](eosBlockchain, eosAddress, xtransferAmount, txId, {from: signer.address});
+
+        // xtransfer
+        const xTransfer = await bancorX.connect(signer)['xTransfer(bytes32,bytes32,uint256,uint256)'](eosBlockchain, eosAddress, xtransferAmount, xtransferId, {from: signer.address});
         console.log(xTransfer);
+
+        const balanceAfter = (await bbsToken.balanceOf(signer.address));
+        console.log('BBS balance after x transfer', balanceAfter);
+
+        if (balanceAfter._hex !== '0x00') {
+            throw new Error('balance should be 0');
+        }
+
+        // reportTx
+        const txId = Math.floor(Math.random() * (100000));
+        const reportTx = await bancorX.connect(reporter)['reportTx(bytes32,uint256,address,uint256,uint256)'](eosBlockchain, txId, signer.address, xtransferAmount, xtransferId, {
+            from: reporter.address
+        });
+        console.log(reportTx);
+
+        const endBalance = (await bbsToken.balanceOf(signer.address));
+        console.log('BBS balance after reoprt tx', endBalance);
+
+        if (endBalance._hex !== mintAmount._hex) {
+            throw new Error('balance should be the same as the mint amount');
+        }
+
     });
 });
