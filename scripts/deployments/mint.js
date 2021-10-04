918e586cecc6c0fcaeac673405b88a7b64dc83a2
yanivy <yanivy@freeports.dev>
2021-10-04 14:29:32 +0300

    handle reoccuring executions of the scripts

--- scripts/deployments/mint.js
+++ scripts/deployments/mint.js
@@ -15,6 +15,12 @@ async function main() {
     const bbsToken = Token.attach(BBS_TOKEN_ADDRESS);
     const amountWei = hardhat.ethers.utils.parseEther(config.mint.amount);
 
+    const bbsTokenTotalSupply = await bbsToken.totalSupply();
+    if (amountWei.eq(bbsTokenTotalSupply)) {
+        log(`BBS token supply is ${bbsTokenTotalSupply}. tokens werte already minted. aborting.`);
+        return;
+    }
+
     log(`---Minting ${config.mint.amount} tokens... | ${new Date()}---`);
     await bbsToken.mint(config.mint.to, amountWei);
 
