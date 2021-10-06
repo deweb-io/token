752f8210da8a76bffde033aebc875bc50a462b93
yanivy <yanivy@freeports.dev>
2021-10-06 09:36:41 +0300

    add stacking imp verfication file

--- scripts/deployments/config.js
+++ scripts/deployments/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    network: 'ropsten',
+    network: 'ropsten', // For etherscan verification
     bridge: {
         maxLockLimit : '40000000000000000000000',
         maxReleaseLimit: '80000000000000000000000',
@@ -14,7 +14,7 @@ module.exports = {
     },
     mint: {
         amount: '500000000',
-        to: '0x44569Aa35Ff6d97e6531880712a41D2af72a007C' //BBS TOKEN deployer address
+        to: '0x44569Aa35Ff6d97e6531880712a41D2af72a007C' // BBS TOKEN deployer address
     },
     rewards: {
         quartes: [0, 1, 2, 3, 4],
