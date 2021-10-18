d2c8a8d568593a5541daaa1c2747aaf1ca95a5c2
yanivy <yanivy@freeports.dev>
2021-10-18 14:00:33 +0300

    add multisend calls to gas price script

--- gas_price_deployment.sh
+++ gas_price_deployment.sh
@@ -1,5 +1,4 @@
-# this script is calculating gas prices for contracts deployment and report tx
-
+# Calculates estimation of total deployment fee on ethereum
 CYAN='\033[1;36m'
 GREEN='\033[0;32m'
 NC='\033[0m'
@@ -29,8 +28,8 @@ echo $eth_usd_price USD
 
 echo -e "${CYAN}Avg gas price from ethgasstation${NC}"
 ethgasstation_response=$(curl -s https://ethgasstation.info/api/ethgasAPI.json)
-avg_gas_price="$(expr $(from_json .average "$ethgasstation_response") / 10)" # divide by 10 to get price in Gwei: https://docs.ethgasstation.info/gas-price#gas-price
-echo $avg_gas_price Gwei
+avg_gas_price_in_gwei="$(expr $(from_json .average "$ethgasstation_response") / 10)" # divide by 10 to get price in Gwei: https://docs.ethgasstation.info/gas-price#gas-price
+echo $avg_gas_price_in_gwei Gwei
 
 
 # get gas report using hardhat
@@ -41,26 +40,28 @@ gas_report_staking="$(REPORT_GAS=true npx hardhat test test/staking-test.js 2>/d
 totalFeeETH="0.0"
 totalFeeUSD="0.0"
 
+# $1 is the number of gas units
+accumulateFee() {
+    gas_price_eth="$(calc_gas_price_in_eth $1 $avg_gas_price_in_gwei)"
+    gas_price_usd="$(calc_gas_price_in_usd $gas_price_eth $eth_usd_price)"
+    echo $1 gas units "|" "0"$gas_price_eth ETH "|" $gas_price_usd USD
+    totalFeeETH=$(add $totalFeeETH "0"$gas_price_eth)
+    totalFeeUSD=$(add $totalFeeUSD $gas_price_usd)
+}
+
 getDeploymentFee() {
     echo -e "${CYAN}{$2} expected deployment gas price${NC}"
     contract_deployment_gas_unit="$(echo "'$1'" | awk '$2 == "'$2'" {print $8}')"
-    contract_deployment_gas_price_eth="$(calc_gas_price_in_eth $contract_deployment_gas_unit $avg_gas_price)"
-    contract_deployment_gas_price_usd="$(calc_gas_price_in_usd $contract_deployment_gas_price_eth $eth_usd_price)"
-    echo $contract_deployment_gas_unit gas units "|" "0"$contract_deployment_gas_price_eth ETH "|" $contract_deployment_gas_price_usd USD
-    totalFeeETH=$(add $totalFeeETH "0"$contract_deployment_gas_price_eth)
-    totalFeeUSD=$(add $totalFeeUSD $contract_deployment_gas_price_usd)
+    accumulateFee $contract_deployment_gas_unit
 }
 
 getFunctionCallFee() {
     echo -e "${CYAN}{$2} expected function call gas price${NC}"
     function_call_gas_unit="$(echo "$1" | awk '$4 == "'$2'" {print $10}')"
-    function_call_gas_price_eth="$(calc_gas_price_in_eth $function_call_gas_unit $avg_gas_price)"
-    function_call_gas_price_usd="$(calc_gas_price_in_usd $function_call_gas_price_eth $eth_usd_price)"
-    echo $function_call_gas_unit gas units "|" "0"$function_call_gas_price_eth ETH "|" $function_call_gas_price_usd USD
-    totalFeeETH=$(add $totalFeeETH "0"$function_call_gas_price_eth)
-    totalFeeUSD=$(add $totalFeeUSD $function_call_gas_price_usd)
+    accumulateFee $function_call_gas_unit
 }
 
+# Own contracts #
 getDeploymentFee "$gas_report_bbs_token" "BBSToken"             # BBS token deploy
 getFunctionCallFee "$gas_report_bbs_token" "mint"               # BBS minting
 getFunctionCallFee "$gas_report_bbs_token" "transferOwnership"  # transfer BBS ownership to Gnosis
@@ -75,6 +76,13 @@ getFunctionCallFee "$gas_report_staking" "declareReward"        # decalre reward
 getFunctionCallFee "$gas_report_staking" "declareReward"        # decalre rewards Q3
 getFunctionCallFee "$gas_report_staking" "declareReward"        # decalre rewards Q4
 getFunctionCallFee "$gas_report_bbs_token" "transferOwnership"  # transfer Staking ownership to Gnosis
+
+# Third parties #
+echo -e "${CYAN}{Multisend-approve} expected function call gas price${NC}"
+accumulateFee '46274'  # Multisend approve (https://ropsten.etherscan.io/tx/0xcb813d20378d25e6bf9a3924a5bccaaa4fd7cd60781c25d61751ff1ca09a62f2)
+
+echo -e "${CYAN}{Multisend-transfer} expected function call gas price${NC}"
+accumulateFee '110484' # Multisend transfer (https://ropsten.etherscan.io/tx/0xb71446904c619006bf5ca9f06472a45bf573b68911e7530e22d2e17dcbefec26)
 
 echo -e "${GREEN}0"$totalFeeETH" ETH${NC}"
 echo -e "${GREEN}"$totalFeeUSD" USD${NC}"
