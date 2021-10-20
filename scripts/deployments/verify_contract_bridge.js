bd18218531a96a2bb94c085587673fcd154cfd46
yanivy <yanivy@freeports.dev>
2021-10-20 09:34:22 +0300

    separate verifications script

--- scripts/deployments/verify_contract_bridge.js
+++ scripts/deployments/verify_contract_bridge.js
@@ -6,21 +6,6 @@ const log = common.log;
 const BBS_TOKEN_ADDRESS = common.getBBStokenAddress();
 const BRIDGE_ADDRESS = common.getBridgeAddress();
 
-
-log(`---Verify BBS contract---`);
-exec(`npx hardhat verify --network ${config.network} ${BBS_TOKEN_ADDRESS}`, (error, stdout, stderr) => {
-    if (error) {
-        log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        log(`stderr: ${stderr}`);
-        return;
-    }
-    log(`stdout: ${stdout}`);
-    log(`---Verify BBS contract Done---`);
-});
-
 log(`---Verify BRIDGE contract---`);
 exec(`npx hardhat verify --network ${config.network} ${BRIDGE_ADDRESS} '${config.bridge.maxLockLimit}' '${config.bridge.maxReleaseLimit}' '${config.bridge.minLimit}' '${config.bridge.limitIncPerBlock}' ${config.bridge.minRequiredReports} ${config.bridge.commissionAmount} ${BBS_TOKEN_ADDRESS}`, (error, stdout, stderr) => {
     if (error) {
