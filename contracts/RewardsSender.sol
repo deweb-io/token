6c4e22d823e55682ca9de8479e7c6b48d2c75917
yanivy <yanivy@freeports.dev>
2021-12-01 14:58:31 +0200

    rewards-sender: change imports

--- contracts/RewardsSender.sol
+++ contracts/RewardsSender.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./Bridge.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+interface IBridge {
+    function sendRewards(uint256 amount) external;
+}
 
 contract RewardsSender {
-    Bridge bridge;
-    ERC20 bbsToken;
+    IBridge bridge;
+    IERC20 bbsToken;
 
-    constructor(ERC20 _bbsToken, Bridge _bridge) {
+    constructor(IERC20 _bbsToken, IBridge _bridge) {
         bridge = _bridge;
         bbsToken = _bbsToken;
         bbsToken.approve(address(bridge), 2**256 - 1);
