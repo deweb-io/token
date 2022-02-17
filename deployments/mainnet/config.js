206f1a937cd4de5e18651bf20f7c50ed0ee18251
yanivy <yanivy@freeports.dev>
2022-02-17 14:16:07 +0200

    update rewards in mainnet config.js

--- deployments/mainnet/config.js
+++ deployments/mainnet/config.js
@@ -6,7 +6,19 @@ module.exports = {
     "safe" : {
         "address" : "0xeE294a573eA5B77b84880a109FAeD7FdC0136d2D"
     },
-    "rewards": [ { "q" : 0, "amount": "100000"}, { "q" : 1, "amount": "100000"}],
+    "rewards": [
+        { "q" : 0, "amount": "10000000"},
+        { "q" : 1, "amount": "10000000"},
+        { "q" : 2, "amount": "10000000"},
+        { "q" : 4, "amount": "10000000"},
+        { "q" : 5, "amount": "10000000"},
+        { "q" : 6, "amount": "10000000"},
+        { "q" : 7, "amount": "10000000"},
+        { "q" : 8, "amount": "10000000"},
+        { "q" : 9, "amount": "10000000"},
+        { "q" : 10, "amount": "10000000"},
+        { "q" : 11, "amount": "10000000"},
+        { "q" : 12, "amount": "10000000"}],
     "bridge": {
         "maxLockLimit" : "40000",
         "maxReleaseLimit": "80000",
