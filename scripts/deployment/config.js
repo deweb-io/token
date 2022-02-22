131a2162b5ed21c0945d251565a94128f5469300
yanivy <yanivy@freeports.dev>
2022-02-22 13:24:03 +0200

    update mainnet config js

--- scripts/deployment/config.js
+++ scripts/deployment/config.js
@@ -8,14 +8,14 @@ module.exports = {
     },
     "rewards": [ { "q" : 0, "amount": "100000"}, { "q" : 1, "amount": "100000"}],
     "bridge": {
-        "maxLockLimit" : "40000",
-        "maxReleaseLimit": "80000",
-        "minLimit": "1",
-        "limitIncPerBlock": "500",
+        "maxLockLimit" : "357142",
+        "maxReleaseLimit": "357142",
+        "minLimit": "135",
+        "limitIncPerBlock": "1300",
         "minRequiredReports": 1,
-        "commissionAmount": "0",
+        "commissionAmount": "135",
         "reporters": {
-            "addresses": [""],
+            "addresses": ["0x0dAD71Ad92C4770AF7c3c28bc1E4F50e12eE4860"],
             "active": [true]
         }
     }
