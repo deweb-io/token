f39a071a5fdc7f7609858d61d7d61fea1df485ef
yanivy <yaniv@deweb.io>
2022-04-28 16:46:41 +0300

    wip: substaking end-to-end test

--- test/end-to-end-test.js
+++ test/end-to-end-test.js
@@ -86,74 +86,125 @@ describe('End to End', () => {
         ]);
     });
 
-    it('load testing [ @skipOnCoverage ]', async function(){ // Do not use arrow notation or you won't have "this".
-        const iterations = parseInt(process.env.TEST_ITERATIONS, 10) || 3;
-        if(iterations < 1000){
-            console.warn(`NOTE: running load testing with only ${iterations} iterations (set TEST_ITERATIONS)`);
-        }
-        this.timeout(5000 + (iterations * 10000));
-        let stakers = getStakers();
-        let owner = getOwner();
+    it('end to end 3 [ @skipOnCoverage ]', async() => {
+        await runScenario([
+            // Staking:Q0, SubStaking:Null
+            {action: 'declareReward', quartersIdx: range(4), amount: 10**9},
+            {action: 'lock', staker: 'alice', amount: 10**6, unlockQuarter: 3},
+            {action: 'lock', staker: 'bob', amount: 10**6, unlockQuarter: 3},
+            {action: 'lock', staker: 'carol', amount: 10**6, unlockQuarter: 3},
+            {action: 'increaseTimeTo', quarterIdx: 0.5},
+            {action: 'deploySub'},
+            // Staking:Q0, SubStaking:Q0
+            {action: 'lockSub', staker: 'binanceStaker1', amount: (10**6) / 2, unlockQuarter: 3},
+            {action: 'lockSub', staker: 'binanceStaker2', amount: (10**6) / 2, unlockQuarter: 3},
+            {action: 'withdraw', amount: 10**6 },
+            {action: 'lock', staker: 'manager', amount: 10**6, unlockQuarter: 3},
+            {action: 'increaseTimeTo', quarterIdx: 1},
+            // Staking:Q1, SubStaking:Q0
+            {action: 'claim', staker: 'alice', stakeIdx: 0},
+            {action: 'claim', staker: 'manager', stakeIdx: 0}, // claim rewards from Staking:Q0
+            {action: 'claim', staker: 'bob', stakeIdx: 0},
+            {action: 'claim', staker: 'carol', stakeIdx: 0},
+            {action: 'decalreRewardSub', quarterId: 0}, // declare rewards to SubStaking:Q0
+            {action: 'increaseTimeTo', quarterIdx: 2},
+            // Staking:Q2, SubStaking:Q1
+            {action: 'claimSub', 'staker': 'binanceStaker1', stakeIdx: 0}, // claim for cleanup
+            {action: 'claimSub', 'staker': 'binanceStaker2', stakeIdx: 0}, // claim for cleanup
+            {action: 'claim', staker: 'alice', stakeIdx: 0, assertClaimEquals: 250000000},
+            {action: 'lockRewards', staker: 'bob', stakeIdx: 0, assertStakeIncreaseEquals: 250000000},
+            {action: 'extend', staker: 'carol', stakeIdx: 0, unlockQuarter: 5, assertSharesEqual: 10**6 * 150},
+            {action: 'claim', staker: 'manager', stakeIdx: 0, assertClaimEquals: 250000000}, // claim rewards from Staking:Q1
+            {action: 'decalreRewardSub', quarterId: 1}, // declare rewards to SubStaking:Q1
+            {action: 'increaseTimeTo', quarterIdx: 3}, // Staking:Q3, SubStaking:Q2
+            // Alice shares in Q2 = 100 * (10**6) = 100,000,000
+            // Manager shares in Q2 = 100 * (10**6) = 100,000,000
+            // Bob shares in Q2 = 100 * (10**6 + 250000000) = 25100000000
+            // Carol shares in Q2 = 150 * (10**6)
+            // Total shares Q2 = 25450000000
+            // Q2 share price = 10**9 / 25450000000 = 0.0392927308447937 (BBS per share)
+            {action: 'claim', staker: 'alice', stakeIdx: 0, assertClaimEquals: 4929273}, // Q2 rewards + initial stake
+            {action: 'claim', staker: 'bob', stakeIdx: 0, assertClaimEquals: 1237247544}, // Q2 Rewards + Q1 Rewards + initial stake
+            {action: 'claim', staker: 'carol', stakeIdx: 0, assertClaimEquals: 255893909}, // Q2 Rewards + Q1 Rewards
+            {action: 'claim', staker: 'manager', stakeIdx: 0, assertClaimEquals: 3929273 + 10**6}, // Q2 Rewards + initial stake
 
-        // Create/remove stakers as needed.
-        while(stakers.length < iterations) stakers.push(new ethers.Wallet(stakers.length, owner.provider));
-        stakers = stakers.slice(0, iterations);
-
-        // Declare rewards.
-        const rewardAmount = 10**9;
-        const totalRewards = 14 * rewardAmount;
-        await declareReward(range(14), rewardAmount);
-
-        // Lock stakes.
-        const stakeAmount = 10**6;
-        let totalStakes = 0;
-        for(const [index, staker] of stakers.entries()){
-            owner.sendTransaction({to: staker.address, value: ethers.utils.parseEther('0.5')});
-            console.debug(`locking (${index + 1}/${iterations})`);
-            await lock(staker, stakeAmount, 13 - (index % 13));
-            totalStakes += stakeAmount;
-        }
-
-        // Promote to quarter 1, extend locks, and claim all rewards on quarter 0 to clear partial quarters.
-        await increaseTimeTo(1);
-        let totalClaims = 0;
-        for(const [index, staker] of stakers.entries()){
-            console.debug(`extending and claiming rewards of quarter 0 (${index + 1}/${iterations})`);
-            await extend(staker, 0, 14 - (index % 13));
-            totalClaims += await claim(staker, 0);
-        }
-
-        // Go over each quarter, claiming and expecting.
-        const iterationsQuotient = Math.floor(iterations / 13);
-        const iterationsRemainder = 12 - (iterations % 13);
-        for(let quarterShift in range(13)){
-            // Why do I need to parseInt here, javascript?! Why???
-            await increaseTimeTo(parseInt(quarterShift, 10) + 2);
-
-            // Collect data, only asserting consistency.
-            let sampleClaims = [];
-            const claimIterations = iterations - (iterationsQuotient * quarterShift) - (
-                quarterShift > iterationsRemainder ? quarterShift - iterationsRemainder - 1 : 0);
-            let claimIndex = 0;
-            for(const [index, staker] of stakers.entries()){
-                if(index % 13 >= 13 - quarterShift) continue;
-                console.debug(`claiming (${++claimIndex}/${claimIterations})`);
-                let currentClaim = await claim(staker, 0);
-                totalClaims += currentClaim;
-                if(index % 13 === 12 - quarterShift) currentClaim -= stakeAmount;
-                if(sampleClaims.length < 13 - quarterShift) sampleClaims.push(currentClaim);
-                else expect(currentClaim).to.equal(sampleClaims[index % 13]);
-            }
-
-            // Verify that the boosts are correct.
-            let baseClaim = sampleClaims.pop();
-            for(const [index, sample] of Object.entries(sampleClaims)){
-                const expectedRatio = 1 + (0.25 * (sampleClaims.length - index));
-                expect(Math.abs(expectedRatio - (sample / baseClaim))).to.be.below(1);
-            }
-        }
-
-        // Verify totals.
-        expect(totalStakes + totalRewards - totalClaims).to.be.within(0, (iterations - 1) * 13);
+            //In this stage we are in the middle of Substaking Q2 (rewards decalred only for Q0 and Q1, and rewards can be claimed up to Q1 including)
+            {action: 'deposit', 'amount': 10**6 }, // deposit back the original stake amount before Q2 ends on SubStaking
+            {action: 'decalreRewardSub', quarterId: 2}, // declare rewards to SubStaking:Q2
+            {action: 'increaseTimeTo', quarterIdx: 4}, // Staking:Q4, SubStaking:Q3
+            {action: 'claimSub', 'staker': 'binanceStaker1', stakeIdx: 0, assertClaimEquals: 127464636}, //127464636 = (10**6 + 3929273(Q2 reward) + 250000000(Q1 reward))/2
+            {action: 'claimSub', 'staker': 'binanceStaker2', stakeIdx: 0, assertClaimEquals: 127464636}
+        ]);
     });
+
+    // it('load testing [ @skipOnCoverage ]', async function(){ // Do not use arrow notation or you won't have "this".
+    //     const iterations = parseInt(process.env.TEST_ITERATIONS, 10) || 3;
+    //     if(iterations < 1000){
+    //         console.warn(`NOTE: running load testing with only ${iterations} iterations (set TEST_ITERATIONS)`);
+    //     }
+    //     this.timeout(5000 + (iterations * 10000));
+    //     let stakers = getStakers();
+    //     let owner = getOwner();
+
+    //     // Create/remove stakers as needed.
+    //     while(stakers.length < iterations) stakers.push(new ethers.Wallet(stakers.length, owner.provider));
+    //     stakers = stakers.slice(0, iterations);
+
+    //     // Declare rewards.
+    //     const rewardAmount = 10**9;
+    //     const totalRewards = 14 * rewardAmount;
+    //     await declareReward(range(14), rewardAmount);
+
+    //     // Lock stakes.
+    //     const stakeAmount = 10**6;
+    //     let totalStakes = 0;
+    //     for(const [index, staker] of stakers.entries()){
+    //         owner.sendTransaction({to: staker.address, value: ethers.utils.parseEther('0.5')});
+    //         console.debug(`locking (${index + 1}/${iterations})`);
+    //         await lock(staker, stakeAmount, 13 - (index % 13));
+    //         totalStakes += stakeAmount;
+    //     }
+
+    //     // Promote to quarter 1, extend locks, and claim all rewards on quarter 0 to clear partial quarters.
+    //     await increaseTimeTo(1);
+    //     let totalClaims = 0;
+    //     for(const [index, staker] of stakers.entries()){
+    //         console.debug(`extending and claiming rewards of quarter 0 (${index + 1}/${iterations})`);
+    //         await extend(staker, 0, 14 - (index % 13));
+    //         totalClaims += await claim(staker, 0);
+    //     }
+
+    //     // Go over each quarter, claiming and expecting.
+    //     const iterationsQuotient = Math.floor(iterations / 13);
+    //     const iterationsRemainder = 12 - (iterations % 13);
+    //     for(let quarterShift in range(13)){
+    //         // Why do I need to parseInt here, javascript?! Why???
+    //         await increaseTimeTo(parseInt(quarterShift, 10) + 2);
+
+    //         // Collect data, only asserting consistency.
+    //         let sampleClaims = [];
+    //         const claimIterations = iterations - (iterationsQuotient * quarterShift) - (
+    //             quarterShift > iterationsRemainder ? quarterShift - iterationsRemainder - 1 : 0);
+    //         let claimIndex = 0;
+    //         for(const [index, staker] of stakers.entries()){
+    //             if(index % 13 >= 13 - quarterShift) continue;
+    //             console.debug(`claiming (${++claimIndex}/${claimIterations})`);
+    //             let currentClaim = await claim(staker, 0);
+    //             totalClaims += currentClaim;
+    //             if(index % 13 === 12 - quarterShift) currentClaim -= stakeAmount;
+    //             if(sampleClaims.length < 13 - quarterShift) sampleClaims.push(currentClaim);
+    //             else expect(currentClaim).to.equal(sampleClaims[index % 13]);
+    //         }
+
+    //         // Verify that the boosts are correct.
+    //         let baseClaim = sampleClaims.pop();
+    //         for(const [index, sample] of Object.entries(sampleClaims)){
+    //             const expectedRatio = 1 + (0.25 * (sampleClaims.length - index));
+    //             expect(Math.abs(expectedRatio - (sample / baseClaim))).to.be.below(1);
+    //         }
+    //     }
+
+    //     // Verify totals.
+    //     expect(totalStakes + totalRewards - totalClaims).to.be.within(0, (iterations - 1) * 13);
+    // });
 });
--- test/staking-utils.js
+++ test/staking-utils.js
@@ -1,10 +1,12 @@
 /* Utilities for our staking tests. */
 const {expect} = require('chai');
 const {signPermit} = require('../scripts/utils/utils');
-const { network } = require('hardhat');
+const hardhat = require('hardhat');
 
 const deadline = 9999999999;
-let owner, stakers, bbsToken, staking, quarterLength, tokenName;
+
+// Why not const variables which are in utils?
+let owner, stakers, bbsToken, staking, substaking, quarterLength, tokenName;
 
 const initStaking = async () => {
     const BBSToken = await ethers.getContractFactory('BBSToken');
@@ -16,12 +18,21 @@ const initStaking = async () => {
     [owner, ...stakers] = await ethers.getSigners();
 };
 
+const initSubStaking = async () => {
+    const SubStaking = await ethers.getContractFactory('SubStaking');
+    const stakingCurrentQuarter = await staking.currentQuarter();
+    const stakingNextQuarterStart = await staking.nextQuarterStart();
+    substaking = await upgrades.deployProxy(
+        SubStaking,
+        [bbsToken.address, stakingCurrentQuarter, hardhat.ethers.BigNumber.from(stakingNextQuarterStart.add(14*60*60*24))]);
+}
+
 const getTime = async () => {
-    await network.provider.send('evm_mine');
+    await hardhat.network.provider.send('evm_mine');
     const stakingQuarter = await staking.currentQuarter();
     const nextQuarterStart = (await staking.nextQuarterStart()).toNumber();
     const currentTime = ethers.BigNumber.from(
-        (await network.provider.send('eth_getBlockByNumber', ['latest', false])).timestamp).toNumber();
+        (await hardhat.network.provider.send('eth_getBlockByNumber', ['latest', false])).timestamp).toNumber();
     const realQuarter = stakingQuarter + (1 - ((nextQuarterStart - currentTime) / quarterLength));
     return {
         realQuarter: realQuarter,
@@ -33,11 +44,10 @@ const getTime = async () => {
 
 const increaseTimeTo = async(quarterIdx) => {
     let {realQuarter, currentTime, nextQuarterStart} = await getTime();
-
     if(realQuarter > quarterIdx) throw(`can not increase time from ${realQuarter} to ${quarterIdx}`);
 
-    await network.provider.send('evm_increaseTime', [(quarterIdx - realQuarter) * quarterLength]);
-    await network.provider.send('evm_mine');
+    await hardhat.network.provider.send('evm_increaseTime', [(quarterIdx - realQuarter) * quarterLength]);
+    await hardhat.network.provider.send('evm_mine');
     currentTime = (await getTime()).currentTime;
     while(currentTime >= nextQuarterStart){
         await staking.promoteQuarter();
@@ -45,7 +55,18 @@ const increaseTimeTo = async(quarterIdx) => {
     }
 
     realQuarter = (await getTime()).realQuarter;
-    console.debug(`current quarter it is now ${realQuarter} (${quarterIdx} requested)`);
+    console.log(`[staking]: current quarter it is now ${realQuarter} (${quarterIdx} requested)`);
+
+    if (substaking) {
+        let nextQuarterStart = (await substaking.nextQuarterStart()).toNumber();
+        console.log(`[substaking]: nextQuarterStart: ${nextQuarterStart}`);
+        console.log(`[substaking]: current quarter: ${await substaking.currentQuarter()}`)
+        while(currentTime >= nextQuarterStart){
+            await substaking.promoteQuarter();
+            nextQuarterStart = await substaking.nextQuarterStart();
+            console.log(`[substaking]: current quarter:${await substaking.currentQuarter()}`);
+        }
+    }
 };
 
 const mintAndDoAs = async (signer, amount) => {
@@ -53,18 +74,36 @@ const mintAndDoAs = async (signer, amount) => {
     return staking.connect(signer);
 };
 
+const mintAndDoAsSub = async (signer, amount) => {
+    await bbsToken.mint(signer.address, amount);
+    return substaking.connect(signer);
+};
+
 const declareReward = async (quartersIdx, rewardAmount) => {
     for(const quarterIdx of quartersIdx){
         const {v, r, s} = await signPermit(owner, staking.address, rewardAmount, deadline, bbsToken, tokenName);
         await (await mintAndDoAs(owner, rewardAmount)).declareReward(
             quarterIdx, rewardAmount, deadline, v, r, s);
-        console.debug(`reward of ${rewardAmount} was declared for quarter ${quarterIdx}`);
+        console.debug(`[Staking]:reward of ${rewardAmount} was declared for quarter ${quarterIdx}`);
     }
+};
+
+const decalreRewardSub = async (quarterId) => {
+    const rewardAmount = await bbsToken.balanceOf(owner.address);
+    await bbsToken.approve(substaking.address, rewardAmount);
+    await substaking.connect(owner)['declareReward(uint16,uint256)'](quarterId, rewardAmount);
+    console.log(`[SubStaking]:reward of ${rewardAmount} was declared for quarter ${quarterId}`);
 };
 
 const lock = async (staker, amount, unlockQuarter) => {
     const {v, r, s} = await signPermit(staker, staking.address, amount, deadline, bbsToken, tokenName);
     await (await mintAndDoAs(staker, amount)).lock(amount, unlockQuarter, deadline, v, r, s);
+    console.debug(`locked ${amount} tokens until ${unlockQuarter} for ${staker.address.slice(0, 5)}`);
+};
+
+const lockSub = async (staker, amount, unlockQuarter) => {
+    const {v, r, s} = await signPermit(staker, substaking.address, amount, deadline, bbsToken, tokenName);
+    await (await mintAndDoAsSub(staker, amount))['lock(uint256,uint16,uint256,uint8,bytes32,bytes32)'](amount, unlockQuarter, deadline, v, r, s);
     console.debug(`locked ${amount} tokens until ${unlockQuarter} for ${staker.address.slice(0, 5)}`);
 };
 
@@ -83,6 +122,16 @@ const lockRewards = async (staker, stakeIdx, assertStakeIncreaseEquals) => {
     console.debug(`restaked ${staker.address.slice(0, 5)}/${stakeIdx} for an added ${stakeChange}`);
 };
 
+const withdraw = async(amount) => {
+    await substaking.withdraw(amount);
+    console.debug(`${owner.address} withdrawed ${amount}`);
+}
+
+const deposit = async(amount) => {
+    await bbsToken.connect(owner).transfer(substaking.address, amount);
+    console.debug(`${owner.address} transfered ${amount} to ${substaking.address}`);
+}
+
 const getBalance = async(staker) => {
     return (await bbsToken.balanceOf(staker.address)).toNumber();
 };
@@ -96,20 +145,38 @@ const claim = async (staker, stakeIdx, assertClaimEquals) => {
     return claimAmount;
 };
 
+const claimSub = async (staker, stakeIdx, assertClaimEquals) => {
+    const startingBalance = await getBalance(staker);
+    await substaking.connect(staker).claim(stakeIdx);
+    const claimAmount = (await getBalance(staker)) - startingBalance;
+    if(typeof(assertClaimEquals) === typeof(1)) expect(claimAmount).to.equal(assertClaimEquals);
+    console.debug(`claimed ${staker.address.slice(0, 5)}/${stakeIdx} and got ${claimAmount}`);
+    return claimAmount;
+};
+
 const runScenario = async (steps) => {
     const functions = {
         declareReward: async(step) => await declareReward(step.quartersIdx, step.amount),
+        decalreRewardSub: async(step) => await decalreRewardSub(step.quarterId),
         lock: async(step) => await lock(step.staker, step.amount, step.unlockQuarter),
+        lockSub: async(step) => await lockSub(step.staker, step.amount, step.unlockQuarter),
         increaseTimeTo: async(step) => await increaseTimeTo(step.quarterIdx),
         extend: async(step) => await extend(step.staker, step.stakeIdx, step.unlockQuarter, step.assertSharesEqual),
         lockRewards: async(step) => await lockRewards(step.staker, step.stakeIdx, step.assertStakeIncreaseEquals),
-        claim: async(step) => await claim(step.staker, step.stakeIdx, step.assertClaimEquals)
+        claim: async(step) => await claim(step.staker, step.stakeIdx, step.assertClaimEquals),
+        claimSub: async(step) => await claimSub(step.staker, step.stakeIdx, step.assertClaimEquals),
+        withdraw: async(step) => await withdraw(step.amount),
+        deploySub: async() => await initSubStaking(),
+        deposit: async(step) => await deposit(step.amount)
     };
     const names = {
         alice: stakers[0],
         bob: stakers[1],
         carol: stakers[2],
-        tal: stakers[3]
+        tal: stakers[3],
+        manager: owner,
+        binanceStaker1 : stakers[4],
+        binanceStaker2 : stakers[5],
     };
     for(const [stepIdx, step] of steps.entries()){
         console.debug(`running step ${stepIdx} - ${step.action}`);
