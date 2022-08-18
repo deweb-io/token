0db9f7efa03cc59913441cc1948a3bd16546ed24
yanivy <yaniv@deweb.io>
2022-08-18 10:14:20 +0300

    config update to goreli + use StakingUpgrade1

--- scripts/deployment/config.js
+++ scripts/deployment/config.js
@@ -1,12 +1,9 @@
 module.exports = {
-    "network": "rinkeby",
+    "network": "goreli",
     "mint": {
         "totalSupply": "1000000000"
     },
-    "safe" : {
-        "address" : "0x3B3944D7351c69944DCFEc85651394dC38326a4d"
-    },
-    "rewards": [ { "q" : 0, "amount": "200000"}, { "q" : 1, "amount": "100000"}],
+    "rewards": [ { "q" : 0, "amount": "100000"}, { "q" : 1, "amount": "100000"} ,{ "q" : 2, "amount": "100000"}],
     "bridge": {
         "maxLockLimit" : "40000",
         "maxReleaseLimit": "80000",
@@ -18,8 +15,5 @@ module.exports = {
             "addresses": ["0x869fAD6004c59fd426939855062aF6f1378817a4"],
             "active": [true]
         }
-    },
-    "dailyRewards": {
-        "amount": "60000"
     }
 }
